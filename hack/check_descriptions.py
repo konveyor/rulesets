5d1b0291ddf0b2575e4f60baa01cbd19d5766f39
Marek Aufart <maufart@redhat.com>
2024-10-22 12:48:07 +0200

    Script update 1
    Signed-off-by: Marek Aufart <maufart@redhat.com>

--- hack/check_descriptions.py
+++ hack/check_descriptions.py
@@ -7,6 +7,9 @@ import os
 import sys
 import yaml
 
+# Use variable below for automatic rewrite of ruleset files
+rewrite = False
+
 rules_dir = sys.argv[1]
 if not rules_dir or not os.path.isdir(rules_dir):
    print("ERROR: invalid rules directory. Provide it as a first argument to this script.")
@@ -18,29 +21,35 @@ print(rules_dir)
 rulesets = [os.path.join(rules_dir, f) for f in os.listdir(rules_dir) if os.path.isfile(os.path.join(rules_dir, f)) and f.endswith(".yaml") and not f.endswith("ruleset.yaml")]
 
 for ruleset in rulesets:
-  with open(ruleset) as data:
-    try:
-        print
-        print("#######################################################################")
-        print(ruleset)
-        print("#######################################################################")
-        print
-        
-        rules = yaml.safe_load(data)
-        for rule in rules:
-           checked_cnt += 1
-           if rule.get("description") == None or rule["description"] == "":
-              missing_cnt += 1
-              print("  " + rule["ruleID"])
+   rules = dict()
+   with open(ruleset) as data:
+      try:
+         print
+         print("#######################################################################")
+         print(ruleset)
+         print("#######################################################################")
+         print
 
-              # Guess based on when condition
-              if rule.get("when"):
-                tags = rule.get("when").get("builtin.hasTags")
-                if tags and len(tags) == 1:
-                   print("    description might be: %s" % tags[0])
-    except yaml.YAMLError as exc:
+         rules = yaml.safe_load(data)
+         for rule in rules:
+            checked_cnt += 1
+            if rule.get("description") == None or rule["description"] == "":
+               missing_cnt += 1
+               print("  " + rule["ruleID"])
+               # Guess based on when condition
+               if rule.get("when"):
+                 tags = rule.get("when").get("builtin.hasTags")
+                 if tags and len(tags) == 1:
+                    print("    description might be: %s" % tags[0])
+                    if rewrite:
+                       rule['description'] = tags[0]
+      except yaml.YAMLError as exc:
         print(exc)
         exit(1)
+
+   if rewrite:
+      with open(ruleset, 'w') as rulesfile:
+         yaml.dump(rules, rulesfile)
 
 print
 print("Checked %d rules and missing description for %d rules." % (checked_cnt, missing_cnt))
