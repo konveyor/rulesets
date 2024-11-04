e174eb4ad264b942bd2a02f79d35a8acefac4641
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2024-11-04 14:12:11 +0100

    Correct rule
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

--- default/generated/spring-framework/tests/data/core-container/src/main/java/org/konveyor/Main.java
+++ default/generated/spring-framework/tests/data/core-container/src/main/java/org/konveyor/Main.java
@@ -2,14 +2,18 @@ package org.konveyor;
 
 import org.konveyor.beans.Bean;
 
+import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
-import java.beans.Introspector;
+
+import org.springframework.beans.BeanInfoFactory;
+import org.springframework.beans.SimpleBeanInfoFactory;
 
 public class Main {
 
     public static void main(String[] args) throws IntrospectionException {
-        Bean bean = new Bean();
-        Introspector.getBeanInfo(bean.getClass(), Object.class);
+        BeanInfoFactory factory = new SimpleBeanInfoFactory();
+        BeanInfo beanInfo = factory.getBeanInfo(Bean.class);
+        System.out.println(beanInfo.getBeanDescriptor());
     }
 
 }
