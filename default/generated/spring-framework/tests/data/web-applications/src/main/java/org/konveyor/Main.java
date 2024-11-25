eb47aaf8a55429c02e8ce29146633d345bac881b
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2024-11-25 12:00:49 +0100

    Fix rule
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

--- default/generated/spring-framework/tests/data/web-applications/src/main/java/org/konveyor/Main.java
+++ default/generated/spring-framework/tests/data/web-applications/src/main/java/org/konveyor/Main.java
@@ -1,5 +1,6 @@
 package org.konveyor;
 
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -9,6 +10,8 @@ public class Main {
     public static void main(String[] args) {
         RestTemplate rest = new RestTemplate();
         rest.getForEntity(URI.create("http://www.example.com/"), Object.class);
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
     }
 
 }
