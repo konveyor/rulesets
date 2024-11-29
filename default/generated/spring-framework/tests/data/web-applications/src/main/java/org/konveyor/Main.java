90f2f64af770c4dbf509b2c057c09d08cba51deb
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2024-11-29 11:49:14 +0100

    Test data fix
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

--- default/generated/spring-framework/tests/data/web-applications/src/main/java/org/konveyor/Main.java
+++ default/generated/spring-framework/tests/data/web-applications/src/main/java/org/konveyor/Main.java
@@ -3,13 +3,12 @@ package org.konveyor;
 import org.springframework.web.client.RestTemplate;
 
 import javax.xml.transform.Source;
-import java.beans.IntrospectionException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class Main {
 
-    public static void main(String[] args) throws IntrospectionException, URISyntaxException {
+    public static void main(String[] args) throws URISyntaxException {
         RestTemplate rest = new RestTemplate();
         rest.getForObject(new URI("http://www.example.com/"), Source.class);
         rest.getForObject("http://www.example.com/", Source.class);
