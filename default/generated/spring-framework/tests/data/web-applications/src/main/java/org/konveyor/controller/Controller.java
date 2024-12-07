40dbe4137d25f77e23963b507f6b8f5f7e11da8d
Daniel Vaseekaran <dvaseeka@redhat.com>
2024-12-07 12:44:52 -0500

    Create rules for changed HttpMethod class #153

--- default/generated/spring-framework/tests/data/web-applications/src/main/java/org/konveyor/controller/Controller.java
+++ default/generated/spring-framework/tests/data/web-applications/src/main/java/org/konveyor/controller/Controller.java
@@ -2,6 +2,7 @@ package org.konveyor.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpMethod;
 
 @RestController
 public class Controller {
