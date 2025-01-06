a2702cc1c4625f12aa2451542e10372fca2581ee
Paul Ferraro <paul.ferraro@redhat.com>
2025-01-06 16:00:54 +0000

    [234] Create rules detecting use of MicroProfile OpenAPI to facilitate EAP XP5->6 migration.
    Signed-off-by: Paul Ferraro <paul.ferraro@redhat.com>

---
+++ default/generated/eapxp6/test/data/eapxp_microprofile_openapi_4.0/src/main/java/org/konveyor/TestApplication.java
@@ -0,0 +1,13 @@
+package org.konveyor;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * @author Paul Ferraro
+ */
+@ApplicationPath("/test")
+public class TestApplication extends Application {
+
+}
+
