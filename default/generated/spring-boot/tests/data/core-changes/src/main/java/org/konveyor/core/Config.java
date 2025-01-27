6d04bb8adc14323fd873d43e8c6a89a7586d9c9a
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2025-01-27 13:50:36 +0100

    Add Spring security rules
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

--- default/generated/spring-boot/tests/data/core-changes/src/main/java/org/konveyor/core/Config.java
+++ default/generated/spring-boot/tests/data/core-changes/src/main/java/org/konveyor/core/Config.java
@@ -2,7 +2,11 @@ package org.konveyor.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
 
 @Configuration
 @EnableConfigurationProperties(ApplicationProperties.class)
@@ -10,4 +14,15 @@ public class Config {
 
     @Autowired
     private ApplicationProperties applicationProperties;
+
+    @Bean
+    public AuthenticationManagerResolver<ServerHttpRequest> authenticationManagerResolver() {
+        // Custom AuthenticationManagerResolver configuration
+        return new AuthenticationManagerResolver<ServerHttpRequest>() {
+            @Override
+            public AuthenticationManager resolve(ServerHttpRequest context) {
+                return null;
+            }
+        };
+    }
 }
