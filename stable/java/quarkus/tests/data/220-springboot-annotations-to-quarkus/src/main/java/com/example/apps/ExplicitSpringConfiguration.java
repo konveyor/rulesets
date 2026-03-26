7388002f9149c1e4b75bb00d95fcd59c9aeac325
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2026-03-26 11:55:24 +0100

    [#292] Improve annotations-to-quarkus
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

---
+++ stable/java/quarkus/tests/data/220-springboot-annotations-to-quarkus/src/main/java/com/example/apps/ExplicitSpringConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.apps;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.example.apps")
+public class ExplicitSpringConfiguration {
+}
